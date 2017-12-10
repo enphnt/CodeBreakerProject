57b51b1f8089b44725bac7d5ea3c66d069abe59f
Nathan Phennel <nathanphennel@gmail.com>
2017-12-10 19:37:56 +1100

    feat: generates 4-digit answer

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,18 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    setHiddenFields();
 }
 
 //implement new functions here
+
+function setHiddenFields() {
+    attempt.value = 0;
+    answer.value = Math.floor(Math.random() * 10000).toString();
+
+    while (answer.value.length < 4) {
+        answer.value = "0" + "answer.value";
+    }
+}
+
+guess();
